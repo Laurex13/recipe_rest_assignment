6cc328790f53ab4937e362a60e4bad1ddc59a6da
Laurex13 <pritiriti2013@gmail.com>
2021-09-20 10:30:44 +0200

    Initial commit project creation

---
+++ src/main/java/se/lexicon/recipedatabase/RecipeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.recipedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeDatabaseApplication.class, args);
+    }
+
+}
